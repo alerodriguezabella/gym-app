8422b587c489f4db22a46ba81b679af84131feb1
Alejandra Rodriguez <alerodriguezabella@gmail.com>
2023-10-10 18:33:42 +0200

    Add utils function

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -6,9 +6,20 @@ export const exerciseOptions = {
   }
 };
 
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+  }
+};
+
 export const fetchData = async (url, options) => {
   const response = await fetch(url, options)
   const data = await response.json()
-
-  return data
+  if(response.status >= 200 && response.status < 300){
+    return data
+  } else {
+    throw new Error(data.message || 'Something went wrong')
+  }
 }
