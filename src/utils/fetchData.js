d51803f09ebd294ee3bff6a8609e96a54239f48a
Alejandra Rodriguez <alerodriguezabella@gmail.com>
2023-10-10 17:27:08 +0200

    Update utils

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,9 +1,8 @@
 export const exerciseOptions = {
   method: 'GET',
-  params: {limit: '10'},
   headers: {
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
     'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
-    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
   }
 };
 
