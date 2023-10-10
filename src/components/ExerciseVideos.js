2fdef6c0049ac13bc86f78452fbf2ec6349b11ca
Alejandra Rodriguez <alerodriguezabella@gmail.com>
2023-10-10 18:34:08 +0200

    Populate videos component

--- src/components/ExerciseVideos.js
+++ src/components/ExerciseVideos.js
@@ -1,8 +1,13 @@
+import { Box, Typography } from '@mui/material'
 import React from 'react'
 
-const ExerciseVideos = () => {
+const ExerciseVideos = ({exerciseVideos, name}) => {
   return (
-    <div>ExerciseVideos</div>
+    <Box sx={{marginTop: {lg: '200px', xs: '20px'}}} p='20px'>
+      <Typography variant='h3' mb='30px'>
+        Watch <span style={{color: '#ff2526', textTransform:'capitalize'}}>{name}</span> exercise videos
+      </Typography>
+    </Box>
   )
 }
 
