eb9c97ac8b66ffec36ae76f602e2bd20e378dcd2
Alejandra Rodriguez <alerodriguezabella@gmail.com>
2023-10-16 17:29:40 +0200

    Fetch targetMuscleExercises and equipmentExercises

--- src/pages/ExcerciseDetail.js
+++ src/pages/ExcerciseDetail.js
@@ -9,6 +9,9 @@ import SimilarExercises from '../components/SimilarExercises'
 const ExcerciseDetail = () => {
   const [exerciseDetail, setExerciseDetail] = useState({})
   const [exerciseVideos, setExerciseVideos] = useState([])
+  const [targetMuscleExercises, setTargetMuscleExercises] = useState([])
+  const [equipmentExercises, setEquipmentExercises] = useState([])
+
   const {id} = useParams()
 
   useEffect(() => {
@@ -19,8 +22,15 @@ const ExcerciseDetail = () => {
       try {
         const exerciseDetailData = await fetchData(`${exerciseDbUrl}/exercises/exercise/${id}`, exerciseOptions)
         setExerciseDetail(exerciseDetailData)
+
         const exerciseVideosData = await fetchData(`${youtubeSearchUrl}/search?query=${exerciseDetailData.name}`, youtubeOptions)
         setExerciseVideos(exerciseVideosData.contents)
+
+        const targetMuscleExercisesData = await fetchData(`${exerciseDbUrl}/exercises/target/${exerciseDetailData.target}`, exerciseOptions)
+        setTargetMuscleExercises(targetMuscleExercisesData)
+
+        const equipmentExercisesData = await fetchData(`${exerciseDbUrl}/exercises/equipment/${exerciseDetailData.equipment}`, exerciseOptions)
+        setEquipmentExercises(equipmentExercisesData)
       } catch (error) {
         console.log('Error', error)
       }
@@ -34,7 +44,7 @@ const ExcerciseDetail = () => {
     <Box>
       <Detail exerciseDetail={exerciseDetail} />
       <ExerciseVideos exerciseVideos={exerciseVideos} name={exerciseDetail.name}/>
-      <SimilarExercises />
+      <SimilarExercises targetMuscleExercises={targetMuscleExercises} equipmentExercises={equipmentExercises}/>
     </Box>
   )
 }
